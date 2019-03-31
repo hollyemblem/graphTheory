734deeb7f46ce8eec83abef1eee1250a298aa538
hollyemblem <holly.emblem@gmail.com>
2019-03-31 21:55:05 +0100

    initial go at the connectivity algorithm, specified here:
    Discrete Mathematics for Computing, Rod Haggarty
    
    Let G = (V,E) be a graph. The algorithm computes the value of c = c(G), the number of connected components of G:
    
    begin
       V' := V;
       c := 0;
       while V' != empty do
          begin
             Choose y member of V'
             Find all vertices joined to y by some path
             Remove these vertices and y from V' and corresponding edges from E;
             c := c + 1
          end
    end

--- connectivity_algorithm.py
+++ connectivity_algorithm.py
@@ -1,10 +1,14 @@
 graph = {"a": ["c"],
-         "b": ["c", "e", "d"],
+         "b": ["c", "e"],
          "c": ["a", "b", "e", ],
-         "d": ["b"],
+         "d": [],
          "e": ["c", "b"],
-         "f": []
+         "f": [],
+         "g": [],
+         "h": []
          }
+
+
 
 
 ## graph logic pinched from https://www.python-course.eu/graphs_python.php
@@ -17,7 +21,6 @@ def generate_edges(graph):
 
     return edges
 
-
 variable = (generate_edges(graph))
 
 # go through graph and get key values
@@ -28,12 +31,12 @@ counter = 0
 
 for key in list(graph):
     if ((graph.get(key, ""))):
-        counter = counter + 1
         variables = (graph.get(key, ""))
         if variables:
             for i in variables:
                 variables_set_two = ((graph.get(i, "")))
                 if variables_set_two:
+                    counter = counter + 1
                     for v in variables_set_two:
                         try:
                             graph.pop(v)
@@ -43,11 +46,12 @@ for key in list(graph):
                         graph.pop(i)
                     except Exception:
                         pass
-
                 try:
                     graph.pop(key)
                 except Exception:
 
                     pass
 
+if (len(graph) == 0):
+    counter = 1
 print(len(graph) + counter)
